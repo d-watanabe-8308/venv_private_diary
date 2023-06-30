751a1e897febcd9cec9664db5a2545beca946f45
d-watanabe <pitagora.3.swich@gmail.com>
2023-07-01 02:44:46 +0900

    フォーム機能の追加

--- diary/forms.py
+++ diary/forms.py
@@ -1,4 +1,7 @@
+import os
+
 from django import forms
+from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前', max_length=30)
@@ -18,3 +21,21 @@ class InquiryForm(forms.Form):
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
 
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = 'お問い合わせ {}'.format(title)
+        message_body = '送信者名: {0}\nメールアドレス: {1}\nメッセージ:\n{2}'.format(name, email, message)
+        form_email = os.environ.get('FROM_EMAIL')
+        to_list = [
+            os.environ.get('TO_EMAIL')
+        ]
+        cc_list = [
+            email
+        ]
+
+        message = EmailMessage(subject=subject, body=message_body, from_email=form_email, to=to_list, cc=cc_list)
+        message.send()
