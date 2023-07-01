303044b91c873cc6c2c275f5411a877b3522560a
d-watanabe <pitagora.3.swich@gmail.com>
2023-07-02 00:30:39 +0900

    管理サイトから日記投稿まで

--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,11 @@
 import logging
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import InquiryForm
 from django.contrib import messages
+from .models import Diary
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +22,11 @@ class InquiryView(generic.FormView):
         messages.success(self.request, 'メッセージを送信しました。 ')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = 'diary_List.html'
+
+    def get_queryset(self):
+        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
+        return diaries
