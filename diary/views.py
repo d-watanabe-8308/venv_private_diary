9b7ff11ae8bc055244f0fbb1f4920c001d16a72f
d-watanabe <pitagora.3.swich@gmail.com>
2023-07-02 12:24:55 +0900

    アクセス制御を追加

--- diary/views.py
+++ diary/views.py
@@ -1,17 +1,30 @@
 import logging
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import InquiryForm
-from django.contrib import messages
-from .models import Diary
+
 from .forms import InquiryForm, DiaryCreateForm
+from .models import Diary
+from django.shortcuts import get_object_or_404
 
 logger = logging.getLogger(__name__)
 
+
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        # URLに埋め込まれた主キーから日記データを1件取得。取得できなかった場合は404エラー。
+        diary = get_object_or_404(Diary, pk=self.kwargs['pk'])
+        # ログインユーザーと日記の作成ユーザーを比較し、異なればraise_exceptionの設定に従う
+        return self.request.user == diary.user
+
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
 
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
@@ -24,6 +37,7 @@ class InquiryView(generic.FormView):
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
 
+
 class DiaryListView(LoginRequiredMixin, generic.ListView):
     model = Diary
     template_name = 'diary_List.html'
@@ -33,9 +47,11 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
 
-class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+
+class DiaryDetailView(LoginRequiredMixin, OnlyYouMixin, generic.DetailView):
     model = Diary
     template_name = 'diary_detail.html'
+
 
 class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
     model = Diary
@@ -49,11 +65,13 @@ class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
         diary.save()
         messages.success(self.request, '日記を作成しました。 ')
         return super().form_valid(form)
+
     def form_invalid(self, form):
         messages.error(self.request, '日記の作成に失敗しました。 ')
         return super().form_invalid(form)
 
-class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
+
+class DiaryUpdateView(LoginRequiredMixin, OnlyYouMixin, generic.UpdateView):
     model = Diary
     template_name = 'diary_update.html'
     form_class = DiaryCreateForm
@@ -69,7 +87,8 @@ class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
         messages.error(self.request, "日記の更新に失敗しました。 ")
         return super().form_invalid(form)
 
-class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+
+class DiaryDeleteView(LoginRequiredMixin, OnlyYouMixin, generic.DeleteView):
     model = Diary
     template_name = 'diary_delete.html'
     success_url = reverse_lazy('diary:diary_list')
